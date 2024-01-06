f3c7b16b93c164a932a2e04f1af10026ec8ec9e5
haonqph37292 <155794714+haonqph37292@users.noreply.github.com>
2024-01-06 22:48:29 +0700

    first commit

---
+++ Assets/CloudMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+    public float speed = 5f; // Tốc độ di chuyển của đám mây
+
+    void Update()
+    {
+        // Di chuyển đám mây theo hướng x với tốc độ đã định
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        // Nếu đám mây đi ra khỏi màn hình, đặt lại vị trí để nó quay lại từ bên trái
+        if (transform.position.x > 20f)
+        {
+            transform.position = new Vector3(-20f, transform.position.y, transform.position.z);
+        }
+    }
+}
